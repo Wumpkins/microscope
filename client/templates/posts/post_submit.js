86b48a9cf97f31c08414e81c2c11c98ad4965ba3
Wumpkins <dhc2129@columbia.edu>
2015-05-28 16:51:28 -0400

    added method

---
+++ client/templates/posts/post_submit.js
@@ -0,0 +1,22 @@
+Template.postSubmit.events({
+	'submit form': function(e) {
+		e.preventDefault();
+
+		var temp = $(e.target).find('[name=url]').val();
+
+		if(!((temp.substr(0,7)==="http://") || (temp.substr(0,8)==="https://")))
+			temp = "http://" + temp;
+
+		var post = {
+			title: $(e.target).find('[name=title]').val(),
+			url: temp
+		};
+
+		Meteor.call('postInsert', post, function(error, result){
+			if (error)
+				return alert(error.reason);
+
+			Router.go('postPage', {_id: result._id});
+		});
+	}
+})
